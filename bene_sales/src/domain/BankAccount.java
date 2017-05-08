b4cf8a5d96f4c78921897135c869290f6f7d24db
IchBene <hei18007@spengergasse.at>
2017-05-08 21:45:43 +0200

    BankAccount(customer_id,first_Name,credit)

--- bene_sales/src/domain/BankAccount.java
+++ bene_sales/src/domain/BankAccount.java
@@ -2,10 +2,54 @@ package domain;
 
 public class BankAccount
 {
+	private int customer_id;
+	private String first_Name;
+	private double credit;
 
-	public BankAccount()
+	public BankAccount(int customer_id, String first_Name, double credit)
 	{
-		// TODO Auto-generated constructor stub
+		this.customer_id = customer_id;
+		this.first_Name = first_Name;
+		this.credit = credit;
+	}
+
+	public int getCustomer_id()
+	{
+		return customer_id;
+	}
+
+	public String getFirst_Name()
+	{
+		return first_Name;
+	}
+
+	public double getCredit()
+	{
+		return credit;
+	}
+
+	public void setCustomer_id(int customer_id)
+	{
+		this.customer_id = customer_id;
+	}
+
+	public void setFirst_Name(String first_Name)
+	{
+		this.first_Name = first_Name;
+	}
+
+	public void setCredit(double credit)
+	{
+		this.credit = credit;
+	}
+
+	@Override
+	public String toString()
+	{
+		StringBuilder builder = new StringBuilder();
+		builder.append("BankAccount [customer_id=").append(customer_id).append(", first_Name=").append(first_Name)
+				.append(", credit=").append(credit).append("]");
+		return builder.toString();
 	}
 
 }
